25958accc61edd682d85ad86dc884af090603c89
pbucci <pbucci@gmail.com>
2022-01-11 11:52:23 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,3 @@
-# Frontend install script for Ubuntu
-sudo apt-get update
-
-# install python
-sudo apt-get install software-properties-common
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt-get update
-sudo apt-get install python3.8
-
-# python module pip installs
-pip install celery
-pip install aiohttp
-
 # install nodejs
 sudo apt install nodejs
 
