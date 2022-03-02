7101ebd23ef82efdfbe3580cfb5c78db3567fd37
AJS1NGH <singhalamjeet18@gmail.com>
2022-03-01 16:27:37 -0800

    added more tests

--- backend/alam_test.py
+++ backend/alam_test.py
@@ -1,6 +1,13 @@
 import tasks
 
-query = "password"
-print("Calling Query Search Task with Query: {}".format(query))
-res = tasks.querySearch.delay(query_string="password")
+query1= "password"
+print("Calling Query Search Task with Query: {}".format(query1))
+res = tasks.querySearch.delay(query_string=query1)
 print(res.get())
+print("Query 1 finished")
+
+query2= "bike"
+print("Calling Query Search Task with Query: {}".format(query2))
+res = tasks.querySearch.delay(query_string=query2)
+print(res.get())
+print("Query 2 finished")
