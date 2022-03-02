5541e0459b03815b93309f9cc5675b0157e0ce34
AJS1NGH <singhalamjeet18@gmail.com>
2022-03-02 11:58:56 -0800

    fix todo

--- backend/tasks.py
+++ backend/tasks.py
@@ -212,7 +212,6 @@ def run_query_init(query_string):
 TODO:
 1. As we move towards/away from docs, we need to keep track of which docs have been moved towards/away from
    because those docs should not be show in the ranked documents.
-2. Different cases of sizes of positive and negative sets.
 '''
 @app.task
 def reorient(teleoscope_id: str, positive_docs: list, negative_docs: list, query: str):
