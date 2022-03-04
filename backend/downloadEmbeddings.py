720394e76fa748ec21b918d16a4fc9aca7ac4fa9
AJS1NGH <singhalamjeet18@gmail.com>
2022-03-03 18:34:09 -0800

    change load/download code

--- backend/downloadEmbeddings.py
+++ backend/downloadEmbeddings.py
@@ -2,9 +2,11 @@ import logging
 import pickle as pkl
 import utils
 import numpy as np
+import json
 
 db = utils.connect()
 allPosts = utils.getAllPosts(db, projection={'id':1, 'selftextVector':1, '_id':0}, batching=True, batchSize=10000)
-with open('embeddings.pkl', 'wb') as handle:
-    pkl.dump(allPosts, handle, protocol=pkl.HIGHEST_PROTOCOL)
+np.savez('/home/phb/embeddings/embeddings.npz', posts=allPosts)
+# with open('embeddings.pkl', 'wb') as handle:
+#     pkl.dump(allPosts, handle, protocol=pkl.HIGHEST_PROTOCOL)
 print('Completed')
