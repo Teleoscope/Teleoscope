7d04d424466f983978f72aa88a2f799f7fd1e066
AJS1NGH <singhalamjeet18@gmail.com>
2022-03-04 15:29:29 -0800

    fixing more bugs

--- backend/tasks.py
+++ backend/tasks.py
@@ -11,7 +11,7 @@ import json
 
 import numpy as np
 import tensorflow_hub as hub
-from celery import Celery
+from celery import Celery, Task
 
 # local files
 import auth
@@ -215,7 +215,7 @@ TODO:
    because those docs should not be show in the ranked documents.
 '''
 @app.task
-class reorient(app.Task):
+class reorient(Task):
     
     def __init__(self):
         with open('/home/phb/embeddings/embeddings.pkl', 'rb') as handle:
@@ -281,8 +281,8 @@ class reorient(app.Task):
 
         return 200
 
-app.tasks.register(reorient())
-
+rt = reorient()
+app.register_task(rt)
 # '''
 # TODO:
 # 1. As we move towards/away from docs, we need to keep track of which docs have been moved towards/away from
