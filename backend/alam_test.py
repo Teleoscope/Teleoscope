120c2596c40e86def3d52016f96dce3303cca767
AJS1NGH <singhalamjeet18@gmail.com>
2022-03-22 12:57:08 -0700

    fix testing bug

--- backend/alam_test.py
+++ backend/alam_test.py
@@ -9,7 +9,7 @@ res = tasks.querySearch.delay(query_string=query4, teleoscope_id=tid)
 res = res.get()
 print("MDBSearch task complete with query {}".format(query4))
 
-res = robj.delay(teleoscope_id='a1', positive_docs=['j1f7am', 'j1f2rk'], negative_docs=['j1f71q', 'j1f36t'], query='mom')
+res = robj.delay(teleoscope_id='a1', positive_docs=['j1f7am', 'j1f2rk'], negative_docs=['j1f71q', 'j1f36t'], query='chores')
 print(res.get())
 
 # print("Testing positive docs size 1, empty negative docs")
