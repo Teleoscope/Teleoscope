196ab5828b3dddc88b4e6ad4d54467baa13b6b07
AJS1NGH <singhalamjeet18@gmail.com>
2022-03-22 12:52:58 -0700

    remove time profiling code

--- backend/alam_test.py
+++ backend/alam_test.py
@@ -9,19 +9,6 @@ res = tasks.querySearch.delay(query_string=query4, teleoscope_id=tid)
 res = res.get()
 print("MDBSearch task complete with query {}".format(query4))
 
-'''
-Lets say we end up wanting to commit the following posts:
-
-POSITIVE:
-
-id: j1f2rk
-title: AITA for getting offended when my dad asked me to do chores?
-
-id: j1f1jh
-title: AITA for not washing the dishes?
-
-NEGATIVE:
-'''
 res = robj.delay(teleoscope_id='a1', positive_docs=['j1f7am', 'j1f2rk'], negative_docs=['j1f71q', 'j1f36t'], query='mom')
 print(res.get())
 
--- backend/tasks.py
+++ backend/tasks.py
@@ -92,48 +92,30 @@ class reorient(Task):
 
     def run(self, teleoscope_id: str, positive_docs: list, negative_docs: list, query: str):
         if self.postsCached == False:
-            logging.info('Embeddings not cached, caching embeddings...')
             # Load embeddings
-            s = time.time()
             self.allPostVectors = np.load('/home/phb/embeddings/embeddings.npz', allow_pickle=False)['posts']
             # Load ids
             with open('/home/phb/embeddings/ids.pkl', 'rb') as handle:
                 self.allPostIDs = pickle.load(handle)
+
             self.postsCached = True
-            e = time.time()
-            logging.info(f'Embeddings cached in {e-s} seconds')
-        else:
-            logging.info('Embeddings already cached...')
 
         if self.db is None:
-            logging.info('DB connection not cached, connecting to DB...')
             self.db = utils.connect()
-            logging.info('DB connection cached...')
-        else:
-            logging.info('DB connection already cached...')
 
-        s = time.time()
         queryDocument = self.db.queries.find_one({"query": query, "teleoscope_id": teleoscope_id})
-        e = time.time()
-        total = (e - s)
-        logging.info(f'Query document retrieved in {total} seconds')
 
         # check if stateVector exists
         if 'stateVector' in queryDocument:
             stateVector = np.array(queryDocument['stateVector'])
         else:
             if self.model is None:
-                logging.info('Model not cached, loading model...')
                 self.model = utils.loadModel()
-                logging.info('Model cached...')
-            else:
-                logging.info('Model already cached...')
 
             stateVector = self.model([query]).numpy() # convert query string to vector
 
         # get vectors for positive and negative doc ids using utils.getPostVector function
         # TODO: OPTIMIZE
-        s = time.time()
         posVecs = []
         for pos_id in positive_docs:
             v = utils.getPostVector(self.db, pos_id)
@@ -143,9 +125,6 @@ class reorient(Task):
         for neg_id in negative_docs:
             v = utils.getPostVector(self.db, neg_id) # need -1??
             negVecs.append(v)
-        e = time.time()
-        total = (e - s)
-        logging.info(f'Pos/Neg Vectors retrieved in {total} seconds')
         
         avgPosVec = None
         avgNegVec = None
@@ -169,40 +148,20 @@ class reorient(Task):
         resultantVec = resultantVec / np.linalg.norm(resultantVec)
         qprime = utils.moveVector(sourceVector=stateVector, destinationVector=resultantVec, direction=direction) # move qvector towards/away from feedbackVector
 
-        s = time.time()
         scores = utils.calculateSimilarity(self.allPostVectors, qprime)
-        e = time.time()
-        total = (e - s)
-        logging.info(f'Similarity scores calculated in {total} seconds')
-        s = time.time()
         newRanks = utils.rankPostsBySimilarity(self.allPostIDs, scores)
-        e = time.time()
-        total = (e - s)
-        logging.info(f'Posts ranked in {total} seconds')
 
-        s = time.time()
         # convert to json to upload to gridfs
         dumps = json.dumps(newRanks)
-        e = time.time()
-        total = (e - s)
-        logging.info(f'Dumped json in {total} seconds')
         # upload to gridfs
-        s = time.time()
         fs = GridFS(self.db, "queries")
         obj = fs.put(dumps, encoding='utf-8')
-        e = time.time()
-        total = (e - s)
-        logging.info(f'Uploaded json to gridfs in {total} seconds')
 
-        s =  time.time()
         # update stateVector
         self.db.queries.update_one({"query": query, "teleoscope_id": teleoscope_id}, {'$set': { "stateVector" : qprime.tolist()}})
         # update rankedPosts
         self.db.queries.update_one({"query": query, "teleoscope_id": teleoscope_id}, {'$set': { "ranked_post_ids" : obj}})
-        e = time.time()
-        total = (e - s)
-        logging.info(f'Updated stateVector and rankedPosts in {total} seconds')
 
-        return 200
+        return 200 # TODO: what to return?
 
 robj = app.register_task(reorient())
