0f7d0bb656f14fa9577a430dcd1a9e98aba0dda9
AJS1NGH <singhalamjeet18@gmail.com>
2022-04-15 18:15:24 -0700

    fix auth import

--- backend/App/App.py
+++ backend/App/App.py
@@ -13,7 +13,7 @@ parent_path = dirname(curr_file_path)
 print(parent_path)
 os.environ["PYTHONPATH"] = parent_path
 
-import auth # **make sure to import auth after setting the environment variable**
+from backend import auth # **make sure to import auth after setting the environment variable**
 from warnings import simplefilter
 from celery import Celery
 from dispatch import WebTaskConsumer
--- backend/App/Tasks/utils.py
+++ backend/App/Tasks/utils.py
@@ -16,7 +16,7 @@ from gensim.similarities import SoftCosineSimilarity
 from gensim.utils import simple_preprocess
 
 # local files
-import auth
+from backend import auth
 
 def unpacker(cursor, key):
     for doc in cursor:
