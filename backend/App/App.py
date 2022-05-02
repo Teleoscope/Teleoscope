17940ef9f3a215b7bc003ffb15b7fd139fc244c8
pbucci <pbucci@gmail.com>
2022-05-02 12:31:50 -0700

    Pb merge (#84)
    * remove some utils dependencies for reorient
    
    * add moveVec to reorient
    
    * reorg into folders, change imports
    
    * fix bug in app.py
    
    * delete highest folder
    
    * remove erroneous import
    
    * revert to prev commit without folders
    
    * add topic nb
    
    * add topic nb
    
    * Delete topics_analysis.ipynb
    
    remove duplicate
    
    * fix naming bug
    
    * trying new ide
    
    * full reorg
    
    * fix auth import
    
    * make verbose
    
    * fix import
    
    * add more verbosity for debug
    
    Co-authored-by: AJS1NGH <singhalamjeet18@gmail.com>
    Co-authored-by: cryslee9 <cryslee9@users.noreply.github.com>
    Co-authored-by: cryslee9 <47191675+cryslee9@users.noreply.github.com>
    Co-authored-by: UBC Student <pbucci@students.cs.ubc.ca>

---
+++ backend/App/App.py
@@ -0,0 +1,46 @@
+'''
+- This file is the main entry point for the application.
+- Celery is initialized here via a shell script on the hosting server.
+'''
+import os, sys
+from os.path import dirname, realpath
+import logging
+
+# Set the environment variable for the application
+# This is required so that subfolders can be imported and can import parent folders
+curr_file_path = dirname(realpath(__file__))
+parent_path = dirname(curr_file_path)
+print(parent_path)
+os.environ["PYTHONPATH"] = parent_path
+
+logging.info(parent_path)
+logging.info(sys.path)
+from backend import auth # **make sure to import auth after setting the environment variable**
+from warnings import simplefilter
+from celery import Celery
+from dispatch import WebTaskConsumer
+# ignore all future warnings
+simplefilter(action='ignore', category=FutureWarning)
+
+# url: "amqp://myuser:mypassword@localhost:5672/myvhost"
+CELERY_BROKER_URL = (
+    f'amqp://'
+    f'{auth.rabbitmq["username"]}:'
+    f'{auth.rabbitmq["password"]}@'
+    f'{auth.rabbitmq["host"]}/'
+    f'{auth.rabbitmq["vhost"]}'
+)
+
+app = Celery('App', backend='rpc://', broker=CELERY_BROKER_URL)
+
+app.conf.update(
+    task_serializer='pickle',
+    accept_content=['pickle'],  # Ignore other content
+    result_serializer='pickle',
+)
+
+'''
+App Related Registrations
+'''
+# Add WebTaskConsumer to the Celery app
+app.steps['consumer'].add(WebTaskConsumer)
