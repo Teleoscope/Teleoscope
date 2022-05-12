04789fa59861f359a7f9b59629c5c27218238128
pbucci <pbucci@gmail.com>
2022-05-12 15:21:42 -0700

    adding example pagination to api (#113)
    Co-authored-by: UBC Student <pbucci@students.cs.ubc.ca>

---
+++ frontend/pages/api/cleanposts/[[...cleanposts]].js
@@ -0,0 +1,19 @@
+import { connectToDatabase } from "../../../util/mongodb";
+
+export default async (req, res) => {
+  const { db } = await connectToDatabase();
+  const { cleanposts } = req.query;
+  var queries = [];
+  if (!cleanposts) {
+    queries = await db.collection("clean.posts.v2").find({}).limit(20).toArray();
+  } else if (cleanposts.length == 1) {
+    queries = await db
+    .collection("clean.posts.v2")
+    .find({ $text: { $search: cleanposts[0] } }).limit(50).toArray();
+  } else if (cleanposts.length == 2) {
+    queries = await db
+    .collection("clean.posts.v2")
+    .find({ $text: { $search: cleanposts[0] } }).limit(parseInt(cleanposts[1])).toArray();
+  }
+  res.json(queries);
+};
