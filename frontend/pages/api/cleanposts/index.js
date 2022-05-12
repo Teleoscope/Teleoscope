42ebfb942e488a5541755dcc8d40990d147d94d9
UBC Student <pbucci@students.cs.ubc.ca>
2022-05-12 13:20:16 -0700

    adding example pagination to api

--- frontend/pages/api/cleanposts/index.js
+++
@@ -1,7 +0,0 @@
-import { connectToDatabase } from "../../../util/mongodb";
-
-export default async (req, res) => {
-  const { db } = await connectToDatabase();
-  const queries = await db.collection("clean.posts.v2").find({}).limit(20).toArray();
-  res.json(queries);
-};
