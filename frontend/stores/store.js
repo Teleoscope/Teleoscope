6ffdabafa45a4442368b9b107ecaf1bea4305e0c
Patrick Lee <redundantcomputation@gmail.com>
2022-05-25 15:19:29 -0700

    Clean up redundant action/stores

--- frontend/stores/store.js
+++ frontend/stores/store.js
@@ -1,24 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit'
 
-import Fav from "../actions/fav" 
-import Hide from "../actions/hide" 
 import AddToWorkspace from "../actions/addtoworkspace"
 import SearchTerm from "../actions/searchterm"
 import ActiveTeleoscopeID from "../actions/activeTeleoscopeID"
 import CheckedPosts from "../actions/checkedPosts"
 import Bookmark from "../actions/bookmark"
-import ShowBookmarkedPosts from '../actions/showBookmarkedPosts'
 
 export default configureStore({
   reducer: {
-  	faver: Fav,
-    hider: Hide,
     adder: AddToWorkspace,
     searchTerm: SearchTerm,
     activeTeleoscopeID: ActiveTeleoscopeID,
     checkedPosts: CheckedPosts,
-    bookmarker: Bookmark,
-    shower: ShowBookmarkedPosts
+    bookmarker: Bookmark
   },
 })
 
