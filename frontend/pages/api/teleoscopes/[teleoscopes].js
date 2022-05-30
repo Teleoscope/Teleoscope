1b1b61026bec35effc69750e22018d33fe767a75
Patrick Lee <redundantcomputation@gmail.com>
2022-05-30 12:43:01 -0700

    Pl iss133 (#134)
    * Clean up redundant action/stores
    
    * Fix Teleoscope initlization too many args
    
    * Fix teleoscopes tech debt
    
    * Fix frontend display of teleoscopes

--- frontend/pages/api/teleoscopes/[teleoscopes].js
+++ frontend/pages/api/teleoscopes/[teleoscopes].js
@@ -3,6 +3,6 @@ import { connectToDatabase } from "../../../util/mongodb";
 export default async (req, res) => {
   const { db } = await connectToDatabase();
   const { teleoscopes } = req.query;
-  const teleoscope = await db.collection("queries").findOne({teleoscope_id: teleoscopes});
+  const teleoscope = await db.collection("teleoscopes").findOne({_id: teleoscopes});
   res.json(teleoscope);
 };
