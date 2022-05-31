95de8aeefca8aebe6d50cb8b4cb18f4fd82066ba
Kenny Averna <68562756+kennyav@users.noreply.github.com>
2022-05-31 12:40:18 -0700

    Implement Groups Clean up UI
    I cleaned up the UI for adding and searching tags, and allowed for a variety of colors to be chosen for the tags.

--- frontend/components/LeftMenuBar.js
+++ frontend/components/LeftMenuBar.js
@@ -116,124 +116,136 @@ export default function LeftMenuBar(props) {
           onChange={(e) => setText(e.target.value)}
           style={{ width: "100%", borderRadius: "0 !important" }}
         />
-      <React.Fragment>
-      <Autocomplete
-        value={value}
-        onChange={(event, newValue) => {
-          if (typeof newValue === 'string') {
-            // timeout to avoid instant validation of the dialog's form.
-            setTimeout(() => {
-              toggleOpen(true);
-              setDialogValue({
-                title: newValue,
-                year: '',
-              });
-            });
-          } else if (newValue && newValue.inputValue) {
-            toggleOpen(true);
-            setDialogValue({
-              title: newValue.inputValue,
-              year: '',
-            });
-          } else {
-            setValue(newValue);
-          }
-        }}
-        filterOptions={(options, params) => {
-          const filtered = filter(options, params);
-
-          if (params.inputValue !== '') {
-            filtered.push({
-              inputValue: params.inputValue,
-              title: `Add "${params.inputValue}"`,
-            });
-          }
-
-          return filtered;
-        }}
-        id="Add Tag"
-        options={top100Films}
-        getOptionLabel={(option) => {
-          // e.g value selected with enter, right from the input
-          if (typeof option === 'string') {
-            return option;
-          }
-          if (option.inputValue) {
-            return option.inputValue;
-          }
-          return option.title;
-        }}
-        selectOnFocus
-        clearOnBlur
-        handleHomeEndKeys
-        renderOption={(props, option) => <li {...props}>{option.title}</li>}
-        sx={{ width: 300 }}
-        freeSolo
-        renderInput={(params) => <TextField {...params} label="Add Tag" />}
-      />
-      <Dialog open={open} onClose={handleClose}>
-        <form onSubmit={handleSubmit}>
-          <DialogTitle>Add a new tag</DialogTitle>
-          <DialogContent>
-            <DialogContentText>
-              Did you miss any film in our list? Please, add it!
-            </DialogContentText>
-            <TextField
-              autoFocus
-              margin="dense"
-              id="name"
-              value={dialogValue.title}
-              style={{ width: "100%", borderRadius: "0 !important" }}
-              onChange={(event) =>
+        <React.Fragment>
+          <Autocomplete
+            value={value}
+            onChange={(event, newValue) => {
+              if (typeof newValue === 'string') {
+                // timeout to avoid instant validation of the dialog's form.
+                setTimeout(() => {
+                  toggleOpen(true);
+                  setDialogValue({
+                    title: newValue,
+                    year: '',
+                  });
+                });
+              } else if (newValue && newValue.inputValue) {
+                toggleOpen(true);
                 setDialogValue({
-                  ...dialogValue,
-                  title: event.target.value,
-                })
+                  title: newValue.inputValue,
+                  year: '',
+                });
+              } else {
+                setValue(newValue);
               }
-              label="title"
-              type="text"
-              variant="standard"
-            />
-            <TextField
-              margin="dense"
-              id="name"
-              value={dialogValue.year}
-              style={{ width: "100%", borderRadius: "0 !important" }}
-              onChange={(event) =>
-                setDialogValue({
-                  ...dialogValue,
-                  year: event.target.value,
-                })
-              }
-              label="year"
-              type="number"
-              variant="standard"
-            />
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleClose}>Cancel</Button>
-            <Button type="submit">Add</Button>
-          </DialogActions>
-        </form>
-      </Dialog>
-    </React.Fragment>
+            }}
+            filterOptions={(options, params) => {
+              const filtered = filter(options, params);
 
-    
+              if (params.inputValue !== '') {
+                filtered.push({
+                  inputValue: params.inputValue,
+                  title: `Add "${params.inputValue}"`,
+                });
+              }
+
+              return filtered;
+            }}
+            id="Add Tag"
+            options={top100Films}
+            getOptionLabel={(option) => {
+              // e.g value selected with enter, right from the input
+              if (typeof option === 'string') {
+                return option;
+              }
+              if (option.inputValue) {
+                return option.inputValue;
+              }
+              return option.title;
+            }}
+            style={{ width: "100%", borderRadius: "0 !important" }}
+            selectOnFocus
+            clearOnBlur
+            handleHomeEndKeys
+            renderOption={(props, option) => <li {...props}>{option.title}</li>}
+            sx={{ width: 300 }}
+            freeSolo
+            renderInput={(params) => 
+            <TextField {...params} 
+              label="tags" 
+              variant="filled"
+              placeholder="Add or Search tag..."
+              onKeyDown={(e) => keyChange(e)}
+              style={{ width: "100%", borderRadius: "0 !important" }}/>}
+          />
+          <Dialog open={open} onClose={handleClose}>
+            <form onSubmit={handleSubmit}>
+              <DialogTitle>Add a new tag</DialogTitle>
+              <DialogContent>
+                <DialogContentText>
+                  Input your tag name and select a color to represent that tag!
+                </DialogContentText>
+                <TextField
+                  variant="filled"
+                  placeholder="Add tag name"
+                  //onKeyDown={(e) => keyChange(e)}
+                  //onChange={(e) => setText(e.target.value)}
+                  style={{ width: "100%", borderRadius: "0 !important" }}
+                  autoFocus
+                  margin="dense"
+                  id="name"
+                  value={dialogValue.title}
+                  onChange={(event) =>
+                    setDialogValue({
+                      ...dialogValue,
+                      title: event.target.value,
+                    })
+                  }
+                  label="tag name"
+                  type="text"
+                />
+                <TextField
+                  variant="filled"
+                  label="color"
+                  placeholder="Add query..."
+                  onKeyDown={(e) => keyChange(e)}
+                  style={{ width: "100%", borderRadius: "0 !important" }}
+                  margin="dense"
+                  id="name"
+                  type="color"
+                  value={dialogValue.year}
+                  onChange={(event) =>
+                    setDialogValue({
+                      ...dialogValue,
+                      year: event.target.value,
+                    })
+                  }
+                />
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={handleClose}>Cancel</Button>
+                <Button type="submit">Add</Button>
+              </DialogActions>
+            </form>
+          </Dialog>
+        </React.Fragment>
+
+
         <FormControlLabel
           style={{ marginLeft: 20, marginTop: 10 }}
           control={<Checkbox style={{ marginRight: 10 }} />}
           onChange={() => setBookmarked(!bookmarked)}
           label="Bookmarked Items Only"
         />
-        <FormControlLabel
+        {/* <FormControlLabel
           style={{ marginLeft: 20, marginTop: 10 }}
           control={<Checkbox style={{ marginRight: 10 }} />}
           onChange={() => setTagged(!tagged)}
           label="Tagged Items Only"
-        />
+        /> */}
 
         {bookmarked ? (<PostList data={bookmarked_data} pagination={true} />) : <PostList data={data} pagination={true} />}
-        {tagged ? (<PostList data={tagged_data} pagination={true} />) : (<PostList data={data} pagination={true} />)}
+        {/* {tagged ? (<PostList data={tagged_data} pagination={true} />) : (<PostList data={data} pagination={true} />)} */}
       </Box>
     </div >
   );
