e69b37b7e7b0037acee2084594ab87d682d9d52a
Patrick Lee <redundantcomputation@gmail.com>
2022-06-03 14:51:58 -0700

    revert to using clean.posts.v2

--- frontend/pages/api/cleanposts/[[...cleanposts]].js
+++ frontend/pages/api/cleanposts/[[...cleanposts]].js
@@ -5,14 +5,14 @@ export default async (req, res) => {
   const { cleanposts } = req.query;
   var queries = [];
   if (!cleanposts) {
-    queries = await db.collection("clean.posts.v3").find({}).limit(20).toArray();
+    queries = await db.collection("clean.posts.v2").find({}).limit(20).toArray();
   } else if (cleanposts.length == 1) {
     queries = await db
-    .collection("clean.posts.v3")
+    .collection("clean.posts.v2")
     .find({ $text: { $search: cleanposts[0] } }).limit(50).toArray();
   } else if (cleanposts.length == 2) {
     queries = await db
-    .collection("clean.posts.v3")
+    .collection("clean.posts.v2")
     .find({ $text: { $search: cleanposts[0] } }).limit(parseInt(cleanposts[1])).toArray();
   }
   res.json(queries);
