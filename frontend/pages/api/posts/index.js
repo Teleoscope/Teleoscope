634056c2e5b889c9d55599dc03f9ad68b0050347
Patrick Lee <redundantcomputation@gmail.com>
2022-06-03 14:57:13 -0700

    Fix clean posts v3 bug

--- frontend/pages/api/posts/index.js
+++ frontend/pages/api/posts/index.js
@@ -2,6 +2,6 @@ import { connectToDatabase } from "../../../util/mongodb";
 
 export default async (req, res) => {
   const { db } = await connectToDatabase();
-  const posts = await db.collection("posts").find({}).limit(20).toArray();
+  const posts = await db.collection("clean.posts.v3").find({}).limit(20).toArray();
   res.json(posts);
 };
