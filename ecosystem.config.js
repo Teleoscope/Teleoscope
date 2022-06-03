ea1bf2da104646ef448d562150d57e442c8ba0c8
UBC Student <pbucci@students.cs.ubc.ca>
2022-06-03 15:21:42 -0700

    updating mdb cols

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,13 +2,16 @@
 // npm install -g pm2
 // pm2 start ecosystem.config.js
 // pm2 monit
+const os = require("os");
+console.log("User info:", userInfo);
+
 module.exports = {
   apps: [
     {
       name: "worker",
       cwd: "./backend",
       script: "/usr/local/bin/python3",
-      args: "-m celery -A dispatch worker --loglevel=INFO -n worker.paul@%%h",
+      args: `-m celery -A dispatch worker --loglevel=INFO -n worker.paul${userInfo.uid}@%h`,
       watch: false,
       interpreter: "",
       max_memory_restart: "1G"
