a2b2c047a57c91b101e758dae4662939190449d2
UBC Student <pbucci@students.cs.ubc.ca>
2022-06-06 13:40:19 -0700

    fixing dev bug

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,1 +1,13 @@
-module.exports = {presets: ['@babel/preset-env']}
+module.exports = api => {
+	const isTest = api.env('test');
+	api.cache(true);
+	if (isTest) {
+		return {
+			presets: [['@babel/preset-env', {targets: {node: 'current'}}]],			
+		}
+	}
+	return {
+		        presets: ['next/babel'],
+
+	}
+};
