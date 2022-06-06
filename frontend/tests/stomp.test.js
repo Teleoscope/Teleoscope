635dee8233523717ef4d29fa3e2f92de8e166008
UBC Student <pbucci@students.cs.ubc.ca>
2022-06-06 14:21:31 -0700

    adding a couple of example synchronous tests

--- frontend/tests/stomp.test.js
+++ frontend/tests/stomp.test.js
@@ -1,4 +1,5 @@
-import {client_init, reorient, initialize_teleoscope, save_UI_state, initialize_session} from "../components/Stomp.js";
+import {publish, client_init, reorient, initialize_teleoscope, save_UI_state, initialize_session} from "../components/Stomp.js";
+
 class DummyClient {
 	publish(msg) {
 		console.log(msg);
@@ -6,7 +7,41 @@ class DummyClient {
 	}
 }
 
-test("testing dummy client publish", () => {
+// Syncronous tests all using DummyClient to check return values
+
+// testing the test suite
+test("Testing dummy client publish.", () => {
 	expect(new DummyClient().publish({"msg":"hello"})).toStrictEqual({"msg":"hello"})
 })
 
+// publish
+test("Testing Stomp.js publish with dummy client.", () => {
+	expect(publish(new DummyClient(),{})).toStrictEqual({});
+})
+
+// initialize_session
+test("Testing initialize_session return value.", () => {
+	expect(initialize_session(new DummyClient(), "paul"
+		)).toStrictEqual(
+		{
+			"task" : "initialize_session",
+			"args" : {
+				"username" : "paul"
+			}
+		}
+	);
+})
+
+// initialize_teleoscope
+test("Testing initialize_teleoscope return value.", () => {
+	expect(initialize_teleoscope(new DummyClient(), "test label", 1010
+		)).toStrictEqual(
+		{
+			"task" : "initialize_teleoscope",
+			"args" : {
+				"label" : "test label",
+				"session_id": 1010,
+			}
+		}
+	);	
+})
