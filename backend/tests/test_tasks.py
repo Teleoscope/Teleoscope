3a50f5a5523705ee283b357dfb6374c62423327a
Dhruv Khanna <dhruv.khanna016@gmail.com>
2022-06-08 23:52:13 +0000

    Teleoscope 124: Setup test file, delete unwanted files

---
+++ backend/tests/test_tasks.py
@@ -0,0 +1,14 @@
+import pytest
+import backend.tasks as tasks
+
+# Test for tasks.py
+# Test edge cases for initialize_teleoscope
+# Case 1: Empty label
+def test_initialize_teleoscope_empty_label():
+	return True
+
+# Test edge cases for initialize_session
+
+# Test edge cases for save_teleoscope_state
+
+# Test edge cases for save_UI_state
