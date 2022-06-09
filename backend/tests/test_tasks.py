d3186d6df23bb6141ea7ed4ee02d779c3cb91c90
Dhruv Khanna <dhruv.khanna016@gmail.com>
2022-06-09 00:57:00 +0000

    Minor: Add test case for empty labell

--- backend/tests/test_tasks.py
+++ backend/tests/test_tasks.py
@@ -1,11 +1,16 @@
-import pytest
-import backend.tasks as tasks
+# Hack to import parent directory modules
+import os, sys
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
 
+import pytest
+import tasks
 # Test for tasks.py
 # Test edge cases for initialize_teleoscope
 # Case 1: Empty label
 def test_initialize_teleoscope_empty_label():
-	return True
+	assert tasks.initialize_teleoscope((), label = "") == []
 
 # Test edge cases for initialize_session
 
