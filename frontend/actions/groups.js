fa20f7b46a2f9f27a1d8d69d3eec624b099605cb
Kenny Averna <68562756+kennyav@users.noreply.github.com>
2022-06-22 14:44:52 -0700

    Splice Bug Fix
    Adding untagging a post from a group. The state can now splice a post from the group if it is clicked again.

--- frontend/actions/groups.js
+++ frontend/actions/groups.js
@@ -1,4 +1,5 @@
 // actions.js
+import { speedDialIconClasses } from '@mui/material';
 import { createSlice } from '@reduxjs/toolkit'
 export const Grouped = createSlice({
 	name: 'grouped',
@@ -12,9 +13,14 @@ export const Grouped = createSlice({
 	reducers: {
 		group: (state, action) => {
 			var temp = [...state.grouped_posts];
+			console.log(action.payload);
 			var filter = temp.filter(item => action.payload.id == item.id && action.payload.label == item.label)
 			if (filter.length == 0) {
 				temp.push({id: action.payload.id, label: action.payload.label})	
+				state.grouped_posts = temp;
+			} else {
+				var postIndex = temp.indexOf(action.payload)
+				temp.splice(postIndex, 1);
 				state.grouped_posts = temp;
 			}
 		},
