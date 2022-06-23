ff888518937d68a5d7cee147ecc106645696c7c3
UBC Student <pbucci@students.cs.ubc.ca>
2022-06-23 12:52:01 -0700

    fixed window loading issue

--- frontend/actions/windows.js
+++ frontend/actions/windows.js
@@ -44,9 +44,11 @@ export const Windows = createSlice({
 			state.windows = temp;
 		},
 		loadWindows: (state, action) => {
+			var temp = [...state.windows];
+			console.log(temp)
 			for (var index in action.payload) {
 				var update = action.payload[index];
-				var item = state.windows.find(item => item.i === update.i)
+				var item = temp.find(item => item.i === update.i)
 				if (item) {
 					Object.keys(update).forEach((key, ind) => {
 						if (item.hasOwnProperty(key)) {
@@ -54,9 +56,11 @@ export const Windows = createSlice({
 						}
 					});
 				} else {
-					console.log("drop", update);
+
+					temp.push(update);
 				}
 			}
+			state.windows = temp;
 		}
 	}
 })
