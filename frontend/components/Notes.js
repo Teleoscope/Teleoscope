7041a40cdb3095bbf43c2d7a35cfec0224609840
UBC Student <pbucci@students.cs.ubc.ca>
2022-06-23 15:47:03 -0700

    minor config changes

--- frontend/components/Notes.js
+++ frontend/components/Notes.js
@@ -1,18 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Editor, EditorState} from 'draft-js';
-import 'draft-js/dist/Draft.css';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import {Editor, EditorState} from 'draft-js';
+// import 'draft-js/dist/Draft.css';
+// 
+// export default function MyEditor(props) {
+//   const [editorState, setEditorState] = React.useState(
+//     () => EditorState.createEmpty(),
+//   );
+// 
+//   return (
+//     <div style={{margin: "10px"}}>
+//     <Editor 
+//       editorState={editorState} 
+//       onChange={setEditorState} />
+//     </div>
+//   )
+// }
+// 
 
-export default function MyEditor(props) {
-  const [editorState, setEditorState] = React.useState(
-    () => EditorState.createEmpty(),
+
+import React from "react";
+import { Editor, EditorState } from "draft-js";
+import "draft-js/dist/Draft.css";
+
+export default function MyEditor() {
+  const [editorState, setEditorState] = React.useState(() =>
+    EditorState.createEmpty()
   );
 
+  const editor = React.useRef(null);
+  function focusEditor() {
+    editor.current.focus();
+  }
+
   return (
-    <div style={{margin: "10px"}}>
-    <Editor 
-      editorState={editorState} 
-      onChange={setEditorState} />
+    <div
+      style={{ margin: "10px", minHeight: "6em", cursor: "text" }}
+      onClick={focusEditor}
+    >
+      <Editor
+        ref={editor}
+        editorState={editorState}
+        onChange={setEditorState}
+        // placeholder="Write something!"
+      />
     </div>
-  )
+  );
 }
