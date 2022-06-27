01809fb9f2f115c0f31dde5d1e516122d5cfd012
Kenny Averna <68562756+kennyav@users.noreply.github.com>
2022-06-27 12:03:49 -0700

    Receiving data from MDB in Groups

--- frontend/actions/groups.js
+++ frontend/actions/groups.js
@@ -6,13 +6,13 @@ import { add_group } from "../components/Stomp";
 
 
 const initialState = {
-		groups: {
-			// "label": "#ffffff"
-		},
-		grouped_posts: [
-			// {id: 'wer123', label: 'Red'}
-		],
-		loading: false
+	groups: {
+		// "label": "#ffffff"
+	},
+	grouped_posts: [
+		// {id: 'wer123', label: 'Red'}
+	],
+	loading: false
 }
 
 export const getGroups = createAsyncThunk(
@@ -21,10 +21,10 @@ export const getGroups = createAsyncThunk(
 		const res = await fetch('/api/groups').then(
 
 			(data) => data.json()
-		)	
-	console.log("groups fetch", res)
-	return res
-})
+		)
+		console.log("groups fetch", res)
+		return res
+	})
 
 export const Groups = createSlice({
 	name: 'groups',
@@ -39,7 +39,7 @@ export const Groups = createSlice({
 
 			// if there aren't duplicates then we push, else find index and splice
 			if (filter.length == 0) {
-				temp.push({id: action.payload.id, label: action.payload.label})	
+				temp.push({ id: action.payload.id, label: action.payload.label })
 				state.grouped_posts = temp;
 			} else {
 				var postIndex = temp.indexOf(action.payload)
@@ -48,7 +48,7 @@ export const Groups = createSlice({
 			}
 		},
 		addGroup: (state, action) => {
-			var temp = {...state.groups}; 
+			var temp = { ...state.groups };
 			temp[action.payload.label] = action.payload.color;
 			add_group(action.payload.client, action.payload.label, action.payload.color);
 			state.groups = temp;
@@ -58,11 +58,16 @@ export const Groups = createSlice({
 		[getGroups.pending]: (state) => {
 			state.loading = true;
 		},
-		[getGroups.fulfilled]: (state, {payload}) => {
+		[getGroups.fulfilled]: (state, { payload }) => {
 			state.loading = false
 			var groups = {}
+			var groupedPosts = [];
 			payload.forEach((g) => {
 				groups[g.label] = g.color;
+				var lastItem = g.history[g.history.length - 1];
+				lastItem.included_posts.forEach((i) => {
+					groupedPosts.push({ id: i, label: g.label });
+				})
 			})
 			console.log("groups fulfilled", groups)
 			state.groups = groups;
