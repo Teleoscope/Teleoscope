2763deebd1d61990b237b7ad4b9e35f6de10e066
Patrick Lee <redundantcomputation@gmail.com>
2022-06-27 11:33:12 -0700

    Add function signatures

--- frontend/components/Stomp.js
+++ frontend/components/Stomp.js
@@ -126,6 +126,10 @@ export function add_group(client, label, color) {
   publish(client, body);
 }
 
+export function add_doc_to_group(client, postid) {
+  
+}
+
 export function add_note(client, postid) {
   var body = {
     task: 'add_note',
