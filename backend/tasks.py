f293f66fd7098f72c5d44913fb10deb83b8668af
Patrick Lee <redundantcomputation@gmail.com>
2022-06-27 12:49:01 -0700

    Fix typo

--- backend/tasks.py
+++ backend/tasks.py
@@ -192,7 +192,7 @@ def add_group(*args, **kwargs):
     groups_res = db.groups.insert_one(obj)
     logging.info(f"Added group {obj['history'][0]['label']} with result {groups_res}.")
     sessions_res = db.session.update_one({'_id': _id}, {'$push': {"groups": groups_res.inserted_id}})
-    logging.info(f"Associated group {obj['history'][0]['label']} with session {_id}}.")
+    logging.info(f"Associated group {obj['history'][0]['label']} with session {_id}.")
     return groups_res
 
 '''
