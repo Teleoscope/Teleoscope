6727d95f4f17eb8130a0b565e817d6482507751c
UBC Student <pbucci@students.cs.ubc.ca>
2022-07-07 13:59:05 -0700

    splitting into smaller components

--- frontend/components/WorkspaceItem.js
+++ frontend/components/WorkspaceItem.js
@@ -1,8 +1,7 @@
-import React, { useState, useContext, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import useDimensions from "react-cool-dimensions";
 
 // material ui
-import { makeStyles } from "@mui/material/styles";
 import IconButton from "@mui/material/IconButton";
 import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
@@ -11,15 +10,13 @@ import Grid from "@mui/material/Grid";
 import CardActionArea from '@mui/material/CardActionArea';
 
 // icons
-import CloseIcon from "@mui/icons-material/Close";
 import ExpandLess from "@mui/icons-material/ExpandLess";
 import ExpandMore from "@mui/icons-material/ExpandMore";
 import BorderColorOutlinedIcon from "@mui/icons-material/BorderColorOutlined";
-import CreateIcon from '@mui/icons-material/Create';
 
 // actions
 import { useSelector, useDispatch } from "react-redux"
-import { dragged, addWindow, removeWindow, loadWindows } from "../actions/windows";
+import { dragged, loadWindows } from "../actions/windows";
 import { checker } from "../actions/checkedPosts";
 
 // custom components
@@ -27,40 +24,14 @@ import PostTitle from "./PostTitle"
 import PostText from "./PostText"
 import GroupSelector from "./GroupSelector"
 import Expander from "./Expander"
+import CloseButton from "./CloseButton"
+import NoteButton from "./NoteButton"
 
 //utils
 import useSWRAbstract from "../util/swr"
-import { add_note } from "../components/Stomp.js";
-
-// contexts
-import { StompContext } from '../context/StompContext'
-
-function getSize(w) {
-  if (w < 100) {
-    return "xs"
-  }
-  if (w < 200) {
-    return "sm"
-  }
-  if (w < 300) {
-    return "md"
-  }
-  if (w < 400) {
-    return "lg"
-  }
-  return "xl"
-}
 
 
 export default function WorkspaceItem(props) {
-  const { observe, unobserve, width, height, entry } = useDimensions({
-    // Triggered whenever the size of the target is changed...
-    onResize: ({ observe, unobserve, width, height, entry }) => {
-        unobserve(); // To stop observing the current target element
-        observe(); // To re-start observing the current target element
-    },
-  });
-
   // strangely, this is needed
   const handleClick = (id, index) => {
     if (index < 0) {
@@ -69,16 +40,10 @@ export default function WorkspaceItem(props) {
       dispatch(checker(id))
     }
   }
-
-  const handleAddNote = () => {
-    dispatch(addWindow({i: props.id + "%note", x: 0, y: 0, w: 3, h: 3, type: "Note"}));
-    add_note(client, props.id);
-  }
-
-  const size = getSize(width);
   const container = React.useRef(null);
   const dispatch = useDispatch();
   const checked = useSelector((state) => state.checkedPosts.value);
+  const windows = useSelector((state) => state.windows.windows)
 
   const [open, setOpen] = React.useState(false);
   const [viewMore, setViewMore] = React.useState(false);
@@ -86,45 +51,34 @@ export default function WorkspaceItem(props) {
   const { post, post_loading, post_error } = useSWRAbstract("post", `/api/posts/${props.id}`);
 
   var pc = checked.indexOf(props.id)
-  const client = useContext(StompContext)
 
   const getWidth = () => {
-    if (size == "xs") {
-      return width/20 + "px";
-    } else if (size == "sm") {
-      return width/10 + "px";
-    } else  if (size == "md") {
-      return width/2 + "px";
-    } else {
-      return width / 1.5 + "px";
+    var w = windows.find(i => i.i == props.id);
+    if (w) {
+      return w.w * 50
     }
+    return 100;
   }
 
+
   return (
-    <Grid container spacing={2} ref={observe}>
+    <Grid container spacing={2}>
       <Grid item xs={12}>
-        <Stack 
+        <Stack
           direction="row"
           justifyContent="space-between"
-
         >
           <GroupSelector id={props.id}/>
           <div style={{width: getWidth()}}>
           <CardActionArea onClick={() => handleClick(props.id, pc)}>
-            <PostTitle 
+            <PostTitle
               post={post ? post : {}} 
-              size="sm" 
               noWrap={true}
-              
             />  
           </CardActionArea>
           </div>
-          <IconButton onClick={() => handleAddNote()}>
-            <CreateIcon fontSize="small" />
-          </IconButton>
-          <IconButton size="small" onClick={() => dispatch(removeWindow(props.id))}>
-            <CloseIcon fontSize="small" />
-          </IconButton>
+          <NoteButton id={props.id}/>
+          <CloseButton id={props.id}/>
         </Stack>
       </Grid>
       <Grid item xs={12}>
