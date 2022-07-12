b4ee6663c7573ce58cb124d4b81fe133eab1893f
Kenny Averna <68562756+kennyav@users.noreply.github.com>
2022-07-12 16:35:55 -0700

    Testing for Group API
    Starting tests to see if the api works. Got the connection and the config all figured out!

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  preset: "@shelf/jest-mongodb",
-  // preset: 'ts-jest',
+  //preset: "@shelf/jest-mongodb",
+  preset: 'ts-jest',
   transform: {
     "^.+\\.(js|jsx)$": "babel-jest",
     "\.js$": "<rootDir>/node_modules/babel-jest"
--- frontend/tests/g.test.js
+++ frontend/tests/g.test.js
@@ -2,9 +2,14 @@
 
 
 // Stomp.js imports
+import { Experimental_CssVarsProvider } from "@mui/material";
+import { group } from "../actions/groups.js";
 import { client_init, publish, initialize_session } from "../components/Stomp.js";
 //import axios from 'axios';
 const axios = require('axios');
+
+
+jest.mock("axios");
 
 class DummyClient {
    publish(msg) {
@@ -13,36 +18,65 @@ class DummyClient {
 }
 
 const testGroup = [{
-      _id: "62bded3a66fbe8bc32c68a9a",
-      color: "#17becf",
+   _id: "62bded3a66fbe8bc32c68a9a",
+   color: "#17becf",
+   label: "corporations",
+   history: [{
+      included_posts: [],
+      label: "corporations"
+   },
+   {
+      included_posts: ["esnzs0"],
       label: "corporations",
-      history: [{
-         included_posts: [],
-         label: "corporations"
-      },
-      {
-         included_posts: ["esnzs0"],
-         label: "corporations",
-         action: "Add post to group"
-      }]
-   }];
+      action: "Add post to group"
+   }]
+}];
 
 
 // async API call for a group with a specific group ID
-async function groups(){
-   let res = await axios.get(`http://localhost:3000/api/groups/62bded3a66fbe8bc32c68a9a`).then(response => response);
-   console.log(res)
-   return res;
+async function groups() {
+   try {
+      return await axios.get('http://localhost:3000/api/groups/62bded3a66fbe8bc32c68a9a');
+   } catch (e) {
+      return "Error - axios couldn't run mock API call";
+   }
 }
 
 
-describe('Testing Suite for Group data from MongoDB', () => {
-   test('Test for Group Data Structure' , () => {
-      // publishes the DummyClient and returns the object body
-      initialize_session(new DummyClient(), "paul");
-      expect(groups()).toStrictEqual(testGroup);
-   })
-})
+describe('Testing Suite for Group API', () => {
+   describe("when API call succeeds", () => {
+      it('Test for Group Data Structure', async () => {
+
+         axios.get.mockResolvedValueOnce(testGroup);
+
+         const result = groups();
+         result.then(response => {
+            console.log('Groups data received: ', response);
+         })
+
+         expect(axios.get).toHaveBeenCalledWith('http://localhost:3000/api/groups/62bded3a66fbe8bc32c68a9a');
+         result.then(response => {
+            expect(response).toEqual(testGroup);
+         });
+      });
+   });
+
+   describe("when API call fails", () => {
+      it('When API call is unsuccessful', () => {
+         const message = "Network Error";
+         axios.get.mockRejectedValueOnce(new Error(message));
+
+         // when
+         const result = groups();
+
+         // then
+         expect(axios.get).toHaveBeenCalledWith('http://localhost:3000/api/groups/62bded3a66fbe8bc32c68a9a');
+         result.then(response => {
+            expect(response).toEqual("Error - axios couldn't run mock API call");
+         });
+      });
+   });
+});
 
 
 /* 
