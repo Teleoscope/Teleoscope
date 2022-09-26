2109fc52f6a27bfe728154b3e20e88d5ec0ad497
pbucci <paul_bucci@bcit.ca>
2022-09-26 09:15:27 -0700

    limiting posts to about 100k

--- backend/umaptest.py
+++ backend/umaptest.py
@@ -96,13 +96,15 @@ def cluster_by_groups(group_id_strings):
                        low_memory=True).fit(data, y=labels)
     embedding = fitter.embedding_
 
+    labelnames = [group["history"][0]["label"] for group in groups]
+
     logging.info("Drawing plots...")
     fig, ax = plt.subplots(1, figsize=(14, 10))
     plt.scatter(*embedding.T, s=0.1, c=labels, cmap='Spectral', alpha=1.0)
     plt.setp(ax, xticks=[], yticks=[])
-    cbar = plt.colorbar(boundaries=np.arange(11)-0.5)
-    cbar.set_ticks(np.arange(10))
-    cbar.set_ticklabels([group["history"][0]["label"]])
+    cbar = plt.colorbar(boundaries=np.arange(labelnames + 1)-0.5)
+    cbar.set_ticks(np.arange(len(labelnames)))
+    cbar.set_ticklabels(labelnames)
     plt.title('Clusters');
     fig.savefig('clusters.png', dpi=fig.dpi)
     logging.info("Plots saved.")
