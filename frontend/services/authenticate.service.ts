8ab7f2f6235120f6c94d5aa512dbdb555309af69
Kenny Averna <68562756+kennyav@users.noreply.github.com>
2022-11-15 13:38:43 -0800

    authenticate update

--- frontend/services/authenticate.service.ts
+++ frontend/services/authenticate.service.ts
@@ -1,5 +1,7 @@
 import useSWRAbstract from "../util/swr";
+import Router from 'next/router'
 
+const bcrypt = require('bcryptjs');
 
 export const authenticateService = {
    authenticateHash,
@@ -9,8 +11,34 @@ export const authenticateService = {
 function authenticateHash(username: string, password: string) {
    const { user } = useSWRAbstract("user", `/api/authenticate/${session_id}`);
 
+   if (!user) {
+      Router.push({
+         pathname: '/account/login'
+      })
+      return false;
+   }
+
+   const salt = bcrypt.genSaltSync(10);
+   const hashedPassword = bcrypt.hashSync(password, salt);
+
+   const passCompare = (password === user.password);
+   const userCompare = (username === user.username);
+
+
+   if (passCompare && userCompare) {
+      //Router.push('/')
+      return true;
+   } else {
+      Router.push({
+         pathname: '/account/login',
+         query: {error: true}
+      })
+      return false;
+   }
+
 }
 
 function verifyToken() {
 
 }
+
