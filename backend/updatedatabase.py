38984a9e1fc368e2d1f6e8183bae090aafa0752b
pbucci <paul_bucci@bcit.ca>
2022-11-17 11:13:50 -0800

    finishing update of stompjs

--- backend/updatedatabase.py
+++ backend/updatedatabase.py
@@ -1,53 +1,8 @@
 import utils
 
-db = utils.connect()
-
-users = list(db.users.find({}))
-userids = {}
-for user in users:
-    userids[user["username"]] = user["_id"]
-
-## Array update template
-# db.collection.updateOne(
-#    { <query conditions> },
-#    { <update operator>: { "<array>.$[]" : value } }
-# )
-
-
-
-sessions = db.sessions.find({})
-for session in sessions:
-    history = session["history"]
-    for h in history:
-        if "user" in h:
-            print(type(h["user"]))
-            if type(h["user"]) == type(userids["paul"]):
-                pass
-            elif h["user"] not in userids.keys():
-                h["user"] = userids["paul"]
-            else:
-                h["user"] = userids[h["user"]]
-        else:
-            h["user"] = userids["paul"]
-    print(history)
-    '''uncommment below to actually update'''
-    db.sessions.update_one(
-        {
-            "_id": session["_id"]
-        }, 
-        {
-            "$set": {
-                "history" : history
-            }
-        }
-    )
-    
-
-
 
 # Already ran, don't run again
 def updateSessionSchema():
-    return # although comment this out if you want to
     db = utils.connect()
 
     users = list(db.users.find({}))
@@ -84,4 +39,44 @@ def updateSessionSchema():
 
 
 
-    
+        
+    users = list(db.users.find({}))
+    userids = {}
+    for user in users:
+        userids[user["username"]] = user["_id"]
+
+    ## Array update template
+    # db.collection.updateOne(
+    #    { <query conditions> },
+    #    { <update operator>: { "<array>.$[]" : value } }
+    # )
+
+
+
+    sessions = db.sessions.find({})
+    for session in sessions:
+        history = session["history"]
+        for h in history:
+            if "user" in h:
+                print(type(h["user"]))
+                if type(h["user"]) == type(userids["paul"]):
+                    pass
+                elif h["user"] not in userids.keys():
+                    h["user"] = userids["paul"]
+                else:
+                    h["user"] = userids[h["user"]]
+            else:
+                h["user"] = userids["paul"]
+        print(history)
+        '''uncommment below to actually update
+        db.sessions.update_one(
+            {
+                "_id": session["_id"]
+            }, 
+            {
+                "$set": {
+                    "history" : history
+                }
+            }
+        )
+        '''
