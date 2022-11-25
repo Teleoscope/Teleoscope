c1e20fa70afc02d0f42b1b31f61508e3de156f74
phb <paul_bucci@bcit.ca>
2022-11-25 12:04:18 -0800

    renaming cleanposts (#340)

--- frontend/pages/api/posts/[postid].js
+++ frontend/pages/api/posts/[postid].js
@@ -4,6 +4,6 @@ export default async (req, res) => {
   const client = await clientPromise;
   const db = await client.db('aita');
   const { postid } = req.query;
-  const query_meta = await db.collection("clean.posts.v3").findOne({ id: postid });
+  const query_meta = await db.collection("documents").findOne({ id: postid });
   res.json(query_meta);
 };
