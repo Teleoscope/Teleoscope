eb43629be56abe2b6b240ac9b92aa1b00cdcbb1a
pbucci <paul_bucci@bcit.ca>
2023-02-20 18:04:13 -0800

    more metadata

--- backend/github_docs.py
+++ backend/github_docs.py
@@ -1,11 +1,11 @@
 import pickle
 import tasks
 import schemas
-# import required module
+import json
 import os
 # assign directory
 directory = 'testdata'
- 
+
 # iterate over files in
 # that directory
 for filename in os.listdir(directory):
@@ -16,8 +16,31 @@ for filename in os.listdir(directory):
         o = open(file, "rb")
         data = pickle.load(o)
         for d in data:
-            title = d.title
-            text = d.body
+            title = str(d.title)
+            text = str(d.body)
             textVector = tasks.vectorize_text(text)
-            doc = schemas.create_document_object(title, text, textVector)
+            meta = {
+                "comments": d.comments,
+                "id": d.id,
+                "number": d.number,
+                "state": d.state,
+                "locked": d.locked,
+                "active_lock_reason": d.active_lock_reason,
+                "user": d.user.login
+            }
+            if "created_at" in d:
+                meta["created_at"] = d.created_at.isoformat()
+            if "updated_at" in d:
+                meta["updated_at"] = d.updated_at.isoformat()
+            if "closed_at" in d:
+                meta["closed_at"] = d.closed_at.isoformat()
+            if "assignee" in d:
+                meta["assignee"] = d.assignee.login
+            if "closed_by" in d:
+                meta["closed_by"] = d.closed_by.login
+            if "assignees" in d:
+                meta["assignees"] = [a.login for a in d.assignees]
+         
+
+            doc = schemas.create_document_object(title, text, textVector, meta=meta)
             tasks.add_single_document_to_database(doc)
