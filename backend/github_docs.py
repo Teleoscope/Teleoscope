1291bafeadea8ebb546e9b06bad4e0423e40bb80
pbucci <paul_bucci@bcit.ca>
2023-02-21 05:25:04 -0800

    import

--- backend/github_docs.py
+++ backend/github_docs.py
@@ -19,16 +19,21 @@ for filename in os.listdir(directory):
         data = pickle.load(o)
         acc = 0
         next = 0
+        dontskip = True
         for d in data:
             print(f'Processing Issue {d.id}...')
             acc = acc + 1
-            if acc >= next:
+            #####################################################
+            if acc >= next and dontskip:
                 i = input()
                 if i == "":
                     next = acc + 1
+                elif i == "all":
+                    dontskip = False
                 else:
                     next = acc + int(i)
                 print(f"current: {acc}; next: {next}; i: {i}")
+            #####################################################
             check = list(db.documents.find({"meta.id": d.id}))
             if len(check) > 0:
                 print("Already in DB...")
