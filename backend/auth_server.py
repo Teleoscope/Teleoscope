bd0076edf71042291bbf7ec5751dc98e2a212fd1
Qiyu Zhou <qiyuzhou0131@gmail.com>
2023-02-21 19:00:35 -0800

    Finished login of auth server.

--- backend/auth_server.py
+++ backend/auth_server.py
@@ -1,15 +1,19 @@
-from flask import Flask
-from flask import request
+from flask import Flask, request, make_response
 
 from authenticate import *
 
 app = Flask(__name__)
 
-@app.route("/authenticate", method=['GET'])
+@app.route("/login", methods=['GET'])
 def hello_world():
     if request.method == 'GET':
         username = request.form['username']
         password = request.form['password']
-        if authUser(username, password):
+        auth_value = authUser(username, password)
+        if auth_value == -1:
+            return make_response('Incorrect username', 401)
+        elif auth_value == 0:
+            return make_response('Incorrect password', 403)
+        else:
             token = issue_token(username)
-            # construct a response to send back token
+            return make_response('User authenticated', 200, {'token': token})
--- backend/authenticate.py
+++ backend/authenticate.py
@@ -3,18 +3,26 @@ import bcrypt, datetime, jwt
 import utils
 
 # authenticate user based on provided username and password
-# returns true if authenticated, false otherwise
+# returns -1 if user is not found in database
+# returns 0 if user's password is incorrect
+# returns 1 if user is authenticated
 def authUser(username, password):
     db = utils.connect()
     user_obj = db.users.find_one({'username': username})
-    return bcrypt.checkpw(password, user_obj['password'])
+    if user_obj is None:
+        return -1
+    return bcrypt.checkpw(password.encode('utf-8'), user_obj['password'].encode('utf-8'))
 
 def issue_token(username):
+    curr_time = datetime.datetime.utcnow()
+    # the time are converted into iso format, which is generic in both python and javascript
     payload = {
-        "username": username,
-        "iat": datetime.datetime.utcnow # the time when the token is issued
-        # "eat": # the time when the token is expired
+        'username': username,
+        # the time when the token is issued
+        'iat': curr_time.isoformat(),
+        # the time when the token is expired, expired after one day
+        'eat': (curr_time + datetime.timedelta(days=1)).isoformat()
     }
 
-    token = jwt.encode(payload, "secret", algorithm="HS256")
+    token = jwt.encode(payload, 'secret', algorithm='HS256')
     return token
