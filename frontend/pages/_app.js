8376f45d454852ed3c78199f4879b929ae6aae4c
pbucci <paul_bucci@bcit.ca>
2023-02-22 20:48:07 -0800

    testing removal of account logic

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -4,50 +4,12 @@ import { useRouter } from 'next/router';
 
 import '../styles/global.css';
 
-
-import { userService } from '../services/user.service';
-
 export default App;
 
 function App({ Component, pageProps }) {
     const router = useRouter();
     const [user, setUser] = useState(null);
     const [authorized, setAuthorized] = useState(false);
-
-    // useEffect(() => {
-    //     // on initial load - run auth check 
-    //     authCheck(router.asPath);
-
-    //     // on route change start - hide page content by setting authorized to false  
-    //     const hideContent = () => setAuthorized(false);
-    //     router.events.on('routeChangeStart', hideContent);
-
-    //     // on route change complete - run auth check 
-    //     router.events.on('routeChangeComplete', authCheck)
-
-    //     // unsubscribe from events in useEffect return function
-    //     return () => {
-    //         router.events.off('routeChangeStart', hideContent);
-    //         router.events.off('routeChangeComplete', authCheck);
-    //     }
-    //     // eslint-disable-next-line react-hooks/exhaustive-deps
-    // }, []);
-
-    // function authCheck(url) {
-    //     // redirect to login page if accessing a private page and not logged in 
-    //     setUser(userService.userValue);
-    //     const publicPaths = ['/account/login', '/account/register', '/'];
-    //     const path = url.split('?')[0];
-    //     if (!userService.userValue && !publicPaths.includes(path)) {
-    //         setAuthorized(false);
-    //         router.push({
-    //             pathname: '/account/login',
-    //             query: { returnUrl: router.asPath }
-    //         });
-    //     } else {
-    //         setAuthorized(true);
-    //     }
-    // }
 
     return (
         <>
