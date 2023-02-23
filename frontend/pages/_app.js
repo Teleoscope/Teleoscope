fdb419f0ea20581f4224207a981fe7bc2968a772
pbucci <paul_bucci@bcit.ca>
2023-02-23 14:26:30 -0800

    rebuilding app.js

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -4,9 +4,51 @@ import { useRouter } from 'next/router';
 
 import '../styles/global.css';
 
+
+import { userService } from '../services/user.service';
+
 export default App;
 
 function App({ Component, pageProps }) {
+    const router = useRouter();
+    const [user, setUser] = useState(null);
+    const [authorized, setAuthorized] = useState(true);
+
+    useEffect(() => {
+        // on initial load - run auth check 
+        authCheck(router.asPath);
+
+        // on route change start - hide page content by setting authorized to false  
+        const hideContent = () => setAuthorized(true);
+        router.events.on('routeChangeStart', hideContent);
+
+        // on route change complete - run auth check 
+        router.events.on('routeChangeComplete', authCheck)
+
+        // unsubscribe from events in useEffect return function
+        return () => {
+            router.events.off('routeChangeStart', hideContent);
+            router.events.off('routeChangeComplete', authCheck);
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    function authCheck(url) {
+        // redirect to login page if accessing a private page and not logged in 
+        setUser(userService.userValue);
+        const publicPaths = ['/account/login', '/account/register', '/'];
+        const path = url.split('?')[0];
+        if (!userService.userValue && !publicPaths.includes(path)) {
+            setAuthorized(false);
+            router.push({
+                pathname: '/account/login',
+                query: { returnUrl: router.asPath }
+            });
+        } else {
+            setAuthorized(true);
+        }
+    }
+
     return (
         <>
             <Head>
@@ -15,8 +57,11 @@ function App({ Component, pageProps }) {
             </Head>
 
             <main>
-                <Component {...pageProps} />
+                <div className={`app-container ${user ? 'bg-light' : ''}`}>
+                    {authorized && <Component {...pageProps} />}
+                </div>
             </main>
         </>
+
     );
 }
