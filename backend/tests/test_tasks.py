ad320c0e4789ee6e35586d11eec54ddfbdff05c2
aanandi8 <aanandisidharth2@gmail.com>
2023-03-07 12:35:57 -0800

    Check

--- backend/tests/test_tasks.py
+++ backend/tests/test_tasks.py
@@ -72,9 +72,9 @@ def test_create_child_valid_document():
 		document_id = "637eabe7f0a9482a337a11d5"
 		document = db.documents.find_one({"_id": ObjectId("637eabe7f0a9482a337a11d5")})
 		# reddit_id = document["id"]
-		assert str(reddit_id) == f"{document_id}#{str(start)}#{str(end)}"
+		assert id == f"{document_id}#{str(start)}#{str(end)}"
 	finally:
-		db.documents.delete_one({'_id': id})
+		db.documents.delete_one({'_id': reddit_id})
 #Case 3: valid document id, end_index outside range of document
 def test_create_child_outside_document_range():
 	start = 0
