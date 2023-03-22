389a893c4be051467157442b6d3994b2c519edb5
pbucci <paul_bucci@bcit.ca>
2023-03-22 14:17:31 -0700

    adding parsing args'

--- backend/parser.py
+++ backend/parser.py
@@ -55,7 +55,7 @@ class Pushshift:
         
     def handle(self, obj, args):
         if args.check:
-            self.log(obj['subreddit'])
+            print(obj['subreddit'])
         else:
             self.upload(obj, args)
 
