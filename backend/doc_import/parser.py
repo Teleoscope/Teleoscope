fc10f5d88d71301b1613dc2882167408b2e9bbad
pbucci <paul_bucci@bcit.ca>
2023-03-22 12:28:18 -0700

    python

--- backend/doc_import/parser.py
+++ backend/doc_import/parser.py
@@ -1,6 +1,8 @@
 import zreader
 import ujson as json
 import argparse
+from os import listdir
+from os.path import isfile, join
 
 parser = argparse.ArgumentParser(
                     prog='Pushshift to MongoDB',
@@ -10,18 +12,37 @@ parser = argparse.ArgumentParser(
 parser.add_argument('directory')          # directory to parse
 parser.add_argument('-d', '--database')   # which database to insert into
 parser.add_argument('-s', '--subreddit')  # the subreddit to parse
+parser.add_argument('-k', '--chunk-size') # how large
 parser.add_argument('-c', '--check',
                     action='store_true')  # only print the output rather than inserting into MongoDB
-
-
-# Adjust chunk_size as necessary -- defaults to 16,384 if not specified
-# reader = zreader.Zreader("reddit_data.zst", chunk_size=8192)
 
 # Read each line from the reader
 # for line in reader.readlines():
     # obj = json.loads(line)
 
+def processfile(file, args):
+    reader = zreader.Zreader(file, chunk_size=8192)
+    # Read each line from the reader
+    for line in reader.readlines():
+        obj = json.loads(line)
+        if args.check:
+            print(obj)
+            continue
+
+def process(files, args):
+    for file in files:
+        processfile(file, args)
 
 if __name__ == "__main__":
+    # Parse the arguments
     args = parser.parse_args()
+    # Get all files (not directories) in given directory
+    files = [
+        join(args.directory, f) 
+        for f in listdir(args.directory) 
+        if isfile(join(args.directory, f))
+    ]
+    process(files, args)
+
+    # print arguments
     print(args)
