c1423bab81649d35f4737cd150ab400dd410d864
pbucci <paul_bucci@bcit.ca>
2023-03-23 16:32:01 -0700

    adding vectorizer

--- backend/vectorize_tasks.py
+++ backend/vectorize_tasks.py
@@ -35,7 +35,7 @@ queue = Queue(
 app = Celery('vectorize_tasks', backend='rpc://', broker=CELERY_BROKER_URL)
 app.conf.update(
     task_serializer='pickle',
-    accept_content=['pickle'],  # Ignore other content
+    accept_content=['pickle', 'json'],  # Ignore other content
     result_serializer='pickle',
     task_queues=[queue],
 )
