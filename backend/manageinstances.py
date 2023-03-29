28cd233dc113ecac8c6fa2fc3ca8b79c0491fa8c
pbucci <paul_bucci@bcit.ca>
2023-03-29 12:29:25 -0700

    adding example start/stop instance code

---
+++ backend/manageinstances.py
@@ -0,0 +1,21 @@
+import boto3
+client = boto3.client('ec2')
+
+INSTANCE_IDS: = []
+
+def start_instance():
+    response = client.start_instances(
+        InstanceIds = INSTANCE_IDS # list
+        # AdditionalInfo='string',
+        # DryRun=True|False
+    )
+    return response
+
+def stop_instance():
+    response = client.stop_instances(
+        InstanceIds = INSTANCE_IDS # list
+        # Hibernate=True|False,
+        # DryRun=True|False,
+        # Force=True|False
+    )
+    return response
