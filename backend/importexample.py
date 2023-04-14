a3eec10bd17c2f9b715bb4bfe07e152a393aa206
pbucci <paul_bucci@bcit.ca>
2023-04-14 16:16:46 -0700

    improving import docs and usage

--- backend/importexample.py
+++ backend/importexample.py
@@ -1,15 +1,30 @@
 import utils
 import vectorize_tasks
 import resource
+import argparse
 
-db = utils.connect(db="nursing")
+
+
+parser = argparse.ArgumentParser(
+                    prog='Vectorizer',
+                    description='Create text vectors for a random sample of texts.',
+                    epilog='Still under construction.')
+
+parser.add_argument('-c', '--collection')                     # collection to vectorize
+parser.add_argument('-d', '--database', default="teleoscope") # databse to vectorize
+
+# Configuration fields
+parser.add_argument('-t', '--text', default="text")           # the text field
+parser.add_argument('-s', '--samples', default=100)           # sample size per iteration
+parser.add_argument('-i', '--iterations', default=100)        # number of iterations
 
 
 def get_memory_usage():
     rusage = resource.getrusage(resource.RUSAGE_SELF)
     return rusage.ru_idrss + rusage.ru_isrss  # Virtual memory usage in kilobytes
 
-def runpipe(size):
+def runpipe(size, database):
+  db = utils.connect(db=database)
   filter = {
   "textVector": {
    "$size": 0
@@ -19,10 +34,10 @@ def runpipe(size):
   result = list(db.documents.aggregate(pipeline))
   for doc in result:
     if len(doc["textVector"]) == 0:
-      vectorize_tasks.vectorize_and_upload_text(doc["text"],"nursing",doc["_id"])
+      vectorize_tasks.vectorize_and_upload_text(doc["text"], database, doc["_id"])
 
-for i in range(0, 25):
-  mem_before = get_memory_usage()
-  runpipe(10)
-  mem_after = get_memory_usage()
-  print(f"Virtual memory usage increased by {mem_after - mem_before} KB")
+if __name__ == "__main__":
+  # Parse the arguments
+  args = parser.parse_args()
+  for i in range(0, args.iterations):
+    runpipe(args.samples, args.database)
