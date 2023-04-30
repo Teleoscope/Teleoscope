befb5ec6f962b87aeb4c6333332b8d5597bbcc4e
Qiyu Zhou <qiyuzhou0131@gmail.com>
2023-04-29 18:27:10 -0700

    Code  format correction

--- frontend/middlewares/authDecorator.js
+++ frontend/middlewares/authDecorator.js
@@ -5,21 +5,20 @@ var jwt = require('jsonwebtoken');
 
 function authToken(token) {
     try {
-    // decoded JWT payload
-    const decoded = jwt.verify(token, `${process.env.SECRET_KEY}`);
-    // a string that represents the expiration date
-    const dateStr = decoded['eat'];
-    // an Date object that is the expiration date set in the JWT
-    const expDate = new Date(dateStr);
-    // the current date
-    const currDate = new Date();
+        // decoded JWT payload
+        const decoded = jwt.verify(token, `${process.env.SECRET_KEY}`);
+        // a string that represents the expiration date
+        const dateStr = decoded['eat'];
+        // an Date object that is the expiration date set in the JWT
+        const expDate = new Date(dateStr);
+        // the current date
+        const currDate = new Date();
 
-
-    // check if the JWT is valid
-    if (expDate.getTime() < currDate.getTime()) {
-        return false;
-    }
-    return true;
+        // check if the JWT is valid
+        if (expDate.getTime() < currDate.getTime()) {
+            return false;
+        }
+        return true;
     } catch(err) {
         // the JWT cannot be decoded with the SECRET_KEY, i.e. invalid
         return false;
