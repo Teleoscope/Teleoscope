1a736e68e29e51f4771101d959b8e562b3fed147
leofk <73722423+leofk@users.noreply.github.com>
2023-06-25 15:19:21 -0700

    Update schemas.py

--- backend/schemas.py
+++ backend/schemas.py
@@ -2,7 +2,7 @@ import datetime
 from bson.objectid import ObjectId
 
 
-def create_group_object(color, included_documents, label, action, user_id, description, session_id, cluster_id=ObjectId(str("-1"))):
+def create_group_object(color, included_documents, label, action, user_id, description, session_id, cluster_id=ObjectId(str("0000"))):
     obj = {
         "creation_time": datetime.datetime.utcnow(),
         "sessions": [session_id],
