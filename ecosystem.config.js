39a9a91b03bc2edbdb8db2f754a5e914a8a13ffc
paul <pbucci@cs.ubc.ca>
2023-07-03 17:00:30 -0700

    typo

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,29 +2,15 @@
 // npm install -g pm2
 // pm2 start ecosystem.config.js
 // pm2 monit
-const os = require("os");
-let userInfo = os.userInfo();
-console.log("User info:", userInfo);
 
 module.exports = {
   apps: [
     {
       name: "dispatch",
-      cwd: "./backend",
-      script: "/usr/share/miniconda3/envs/teleoscope/bin/python",
-      args: `-m celery -A dispatch worker --loglevel=INFO -n dispatch.${userInfo.username}@%h`,
+      script: "/usr/share/miniconda3/envs/teleoscope/bin/celery",
+      args: ["-m", "backend.dispatch"],
       watch: false,
-      interpreter: "",
-      max_memory_restart: "1G"
-    },
-    {
-      name: "worker",
-      cwd: "./backend",
-      script: "/usr/share/miniconda3/envs/teleoscope/bin/python",
-      args: `-m celery -A tasks worker --loglevel=INFO -n worker.${userInfo.username}@%h --queues=dev-${userInfo.username}-task`,
-      watch: false,
-      interpreter: "",
-      max_memory_restart: "1G"
+      interpreter: ""
     },
     {
       name: "frontend",
