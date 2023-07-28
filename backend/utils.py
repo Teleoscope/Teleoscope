bf45d9afcaeb6bd976c236a13565cdb342ee7486
leofk <73722423+leofk@users.noreply.github.com>
2023-07-28 16:02:34 -0700

    default

--- backend/utils.py
+++ backend/utils.py
@@ -23,7 +23,7 @@ from . import schemas
 db = "test"
 
 def make_client():
-    autht = "authSource=admin&authMechanism=SCRAM-SHA-256&tls=true"
+    autht = "authSource=admin&authMechanism=SCRAM-SHA-256"
     connect_str = (
         f'mongodb://'
         f'{auth.mongodb["username"]}:'
