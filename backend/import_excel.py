b0864e0021e6b7e8d1ada7523dcab50582503541
phb <pbucci@gmail.com>
2023-11-10 11:34:04 -0800

    one-line change to import

--- backend/import_excel.py
+++ backend/import_excel.py
@@ -23,14 +23,33 @@ try:
 
     for index, row in tqdm(df.iterrows(), total=df.shape[0]):
         doc = db.documents.find_one({"metadata.source_url": row["source_url"]})
+        row_dict = row.to_dict()
         try:
-            db.documents.update_one({"_id": doc["_id"]}, {"$set": {"metadata": row.to_dict()}})
+            db.documents.update_one({"_id": doc["_id"]}, {"$set": {"metadata": row_dict}})
         except errors.WriteError as write_error:
             # Handle the WriteError exception here
             logging.error(f"WriteError: {write_error} for row {row}")
         except errors.ConnectionFailure as connection_failure:
             # Handle the ConnectionError exception here
             logging.error(f"ConnectionError: {connection_failure} for row {row}")
+        except Exception as e:
+            logging.error(f"An unexpected error occurred: {e}. Attempting fix:")
+            db.documents.update_one({"_id": doc["_id"]}, {"$set": {"metadata_new": {}}})
+            for k, v in row_dict.items():
+                try:
+                    logging.error(f"Fixing: key {k} for value {v}...")
+                    db.documents.update_one({"_id": doc["_id"]}, {"$set": {f"metadata_new.{k}": v}})
+                except:
+                    logging.error(f"Fixing: value {v} for key {k} failed in row {index} with {row_dict}. Stringifying...")
+                    db.documents.update_one({"_id": doc["_id"]}, {"$set": {f"metadata_new.{k}": str(v)}})
+            updated_doc = db.documents.find_one({"_id": doc["_id"]})
+            meta_new = updated_doc["metadata_new"]
+            if len(meta_new) == len(row_dict):
+                db.documents.update_one({"_id": doc["_id"]}, {"$set": {f"metadata": meta_new}})
+                db.documents.update_one({"_id": doc["_id"]}, {"$unset": {f"metadata_new": ""}})
+            else:
+                raise Exception(f"Row {index} for {row_dict} failed.")
+            
 except Exception as e:
     # Handle any other unexpected exceptions here
     logging.error(f"An unexpected error occurred: {e}")
