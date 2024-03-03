5b038d5c6d8539c7f0213488bf43e6b67beff17b
phb <pbucci@gmail.com>
2024-03-03 07:26:50 -0800

    file upload

--- frontend/components/Table.tsx
+++ frontend/components/Table.tsx
@@ -2,12 +2,11 @@ import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
 export default function TheTable({ data }) {
-  const headers = data?.length > 0 ? data[0] : [];
+  const headers = data?.length > 0 ? Object.keys(data[0]) : [];
 
   if (!data) {
     return (
@@ -18,11 +17,6 @@ export default function TheTable({ data }) {
   return (
     <TableContainer component={Paper}>
       <Table>
-        <TableHead>
-          <TableRow>
-            {headers.map(header => <TableCell key={header}>{header}</TableCell>)}
-          </TableRow>
-        </TableHead>
         <TableBody>
           {data.map((row, index) => (
             <TableRow key={index}>
