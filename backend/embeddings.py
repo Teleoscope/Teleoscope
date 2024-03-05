8713454f352ab81bb84d2e239fcc69e309417b19
phb <pbucci@gmail.com>
2024-03-04 20:41:22 -0800

    adding to yarn for main folder

--- backend/embeddings.py
+++ backend/embeddings.py
@@ -88,7 +88,7 @@ def milvus_import(*args,
 
 if __name__ == '__main__':
     worker = app.Worker(
-        include=['backend.embeddings', 'embedings'],
+        include=['backend.embeddings', 'embeddings'],
         hostname=f"embeddings.{os.getlogin()}@%h{uuid.uuid4()}",
         loglevel="INFO"
     )
