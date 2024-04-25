19d8a9466cfaf90a2a9e0cc400b630f8f8782805
phb <pbucci@gmail.com>
2024-04-25 14:20:02 -0700

    adding type defs

--- teleoscope.ca/src/app/(corporate)/pricing/page.tsx
+++ teleoscope.ca/src/app/(corporate)/pricing/page.tsx
@@ -9,15 +9,23 @@
 // 456789|123456789|123456789|123456789|123456789|123456789|123456789|1234567890
 ////////////////////////////////////////////////////////////////////////////////
 
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'stripe-pricing-table': any;  // Use `any` or a more specific type if available
+    }
+  }
+}
+
 export default function Pricing() {
   return (
     <main>
       <div>
         <h1>Pricing</h1>
         <script async src="https://js.stripe.com/v3/pricing-table.js"></script>
-<stripe-pricing-table pricing-table-id="prctbl_1P9FT9P5ukdlCTTci88k8GUK"
-publishable-key="pk_live_51OzPc7P5ukdlCTTc7nAVSOfSgAj9s6pfJeD3nCS90kblIazeoYg9sPzEU5QwgjaQ0DIOGw6lLrw7E0qKDzgVQwLs00xo5vIEYB">
-</stripe-pricing-table>
+            <stripe-pricing-table pricing-table-id="prctbl_1P9FT9P5ukdlCTTci88k8GUK"
+                                  publishable-key="pk_live_51OzPc7P5ukdlCTTc7nAVSOfSgAj9s6pfJeD3nCS90kblIazeoYg9sPzEU5QwgjaQ0DIOGw6lLrw7E0qKDzgVQwLs00xo5vIEYB">
+            </stripe-pricing-table>
       </div>
     </main>
   );
