11e48fd0cd9adb5563f7efc9c851ed1f52179827
phb <pbucci@gmail.com>
2024-05-09 13:52:50 -0700

    minor signin/out changes

--- teleoscope.ca/src/app/(product)/dashboard/page.tsx
+++ teleoscope.ca/src/app/(product)/dashboard/page.tsx
@@ -1,11 +1,5 @@
-import { validateRequest } from "@/lib/auth";
-import { redirect } from "next/navigation";
-
 export default async function Dashboard() {
-  const { user } = await validateRequest();
-	if (!user) {
-		return redirect("/signin");
-	}
+
   return (
     <main>
       <div>
