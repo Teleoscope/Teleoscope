1f4684f9c4c79dbb5b4b3af382fc30ddc4598169
Armin Talaie <talaiearmin78@gmail.com>
2024-06-21 16:09:52 -0700

    nav menu options

--- teleoscope.ca/src/components/CorporateMenu.tsx
+++ teleoscope.ca/src/components/CorporateMenu.tsx
@@ -1,61 +1,62 @@
 "use client";
 
 import Link from "next/link";
-
 import {
   NavigationMenu,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
+import { cn } from "@/lib/utils";
+
+const menuItems = [
+  {
+    label: "How It Works",
+    href: "/about",
+  },
+  {
+    label: "Pricing",
+    href: "/pricing",
+  },
+  {
+    label: "Resources",
+    href: "https://teleoscope.ca/",
+  },
+  {
+    label: "Sign In",
+    href: "/signin",
+  },
+  {
+    label: "Get Started",
+    href: "/signup",
+    variant: "primary"
+  },
+];
+
 
 export default function Menu() {
+  const menuStyle = "flex items-center justify-between px-2 gap-4";
+  const menuItemStyle = "flex-shrink-0 p-2 font-medium";
+  const meneItemPrimaryStyle = "flex-shrink-0  bg-pink-600 text-white rounded-sm";
+
   return (
     <div className="flex items-center justify-between">
-      {/* PL: Feels hacky to put all these div classe here - I welcome fixes */}
       <div className="flex-shrink-0 p-4 font-bold">
         <Link href="/" legacyBehavior passHref>
           Teleoscope
         </Link>
       </div>
       <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <Link href="/about" legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                How It Works
+        <NavigationMenuList className={menuStyle}>
+          {menuItems.map((item) => (
+            <NavigationMenuItem key={item.label}>
+            <Link href={item.href} passHref>
+              <NavigationMenuLink className={cn(menuItemStyle, item.variant === "primary" && meneItemPrimaryStyle)}>
+                {item.label}
               </NavigationMenuLink>
             </Link>
           </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Link href="/pricing" legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Pricing
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Link href="https://teleoscope.ca/" legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Docs
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Link href="/signin" legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Sign In
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Link href="/signup" legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Get Started
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
+          ))}
         </NavigationMenuList>
       </NavigationMenu>
     </div>
