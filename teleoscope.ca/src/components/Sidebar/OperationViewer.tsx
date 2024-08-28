6cb8fcc9580871bc0d9158b6f94e1d2ba63cf4a5
phb <pbucci@gmail.com>
2024-08-28 11:04:27 -0700

    syntax error

--- teleoscope.ca/src/components/Sidebar/OperationViewer.tsx
+++ teleoscope.ca/src/components/Sidebar/OperationViewer.tsx
@@ -1,18 +1,56 @@
-import withDataViewer from './withDataViewer';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { Typography, Stack, Divider } from '@mui/material';
+import { useAppSelector } from '@/lib/hooks';
+import DocumentList from '@/components/Documents/DocumentList';
+import WindowDefinitions from '../WindowFolder/WindowDefinitions';
 import { useSWRF } from '@/lib/swr';
 
-const getDataForOperationViewer = (id) => {
+function OperationViewer({ id, type }) {
     const { data: operation } = useSWRF(`/api/graph?uid=${id}`);
-    return {
-        doclists: operation?.doclists,
-        label: operation?.type // or use something else relevant
-    };
-};
+    const { settings } = useAppSelector((state) => state.appState.workspace);
+    const { color } = useAppSelector(
+        (state) => state.appState.workflow.settings
+    );
+    const doclists = operation?.doclists;
+    const label = operation?.type;
 
-const OperationViewer = withDataViewer(
-    (props) => <div />,
-    getDataForOperationViewer,
-    (type) => type
-);
+    const handleLoadMore = () => {
+        console.log('stub');
+    };
+
+    return (
+        <Accordion
+            defaultExpanded={settings?.expanded}
+            disableGutters={true}
+            square={true}
+        >
+            <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel3a-content"
+                id="panel3a-header"
+            >
+                <Typography noWrap align="left">
+                    {WindowDefinitions(type).icon(color)}{' '}
+                    {label ? label : 'Loading...'}
+                </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                <Stack spacing={1} sx={{ margin: '1em' }}>
+                    <Divider></Divider>
+                    <div style={{ height: '25vh' }}>
+                        <DocumentList
+                            data={doclists || []}
+                            pagination={true}
+                            loadMore={handleLoadMore}
+                        ></DocumentList>
+                    </div>
+                </Stack>
+            </AccordionDetails>
+        </Accordion>
+    );
+}
 
 export default OperationViewer;
