652c3b3b2d0dd16fd3213e641d8fcac41315eb57
phb <pbucci@gmail.com>
2024-10-02 15:41:58 -0700

    frigade test

--- teleoscope.ca/src/app/app/layout.tsx
+++ teleoscope.ca/src/app/app/layout.tsx
@@ -1,20 +1,25 @@
-import UserContextProvider from "@/context/UserContext";
-  
-  
-  export default async function DashboardLayout({
-    children,
-  }: {
+import UserContextProvider from '@/context/UserContext';
+import * as Frigade from '@frigade/react';
+
+export default async function DashboardLayout({
+    children
+}: {
     children: React.ReactNode;
-  }) {
+}) {
     return (
-      <div className="flex justify-center w-screen h-screen ">
-        <UserContextProvider userId="1">
-        <div className="flex-1 w-full flex-col">
-          {children}
-          </div>
-        </UserContextProvider>
-      </div>
+        <div className="flex justify-center w-screen h-screen ">
+            <Frigade.Provider
+                apiKey={process.env.FRIGADE_API_PUBLIC || ''}
+                userId="my-user-id"
+            >
+                <UserContextProvider userId="1">
+                    <Frigade.Announcement
+                        flowId="flow_En5edv1a"
+                        dismissible={true}
+                    />
+                    <div className="flex-1 w-full flex-col">{children}</div>
+                </UserContextProvider>
+            </Frigade.Provider>
+        </div>
     );
-  }
-  
-  
+}
