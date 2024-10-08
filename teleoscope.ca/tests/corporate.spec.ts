d47d5d066bbc511dcc5fd1d7d305f977fe0c3f89
phb <pbucci@gmail.com>
2024-10-07 17:00:34 -0700

    playwright

--- teleoscope.ca/tests/corporate.spec.ts
+++ teleoscope.ca/tests/corporate.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('landing page loads', async ({ page }) => {
 
-    await page.goto('http://localhost:3000');
+    await page.goto('http://127.0.0.1:3000');
 
     // Expect page to load
     await expect(page).toHaveTitle(/Teleoscope/);
